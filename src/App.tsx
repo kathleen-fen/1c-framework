9d917deb7a67035f4fa1c12b510ef4af143f4f61
Kate Fenchenko <kathleen750314@gmail.com>
2024-03-23 19:16:14 +0100

    mirage test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+
 import GlobalStyle from "./styles/globalStyle";
+
 import "components/FontawesomeIcons/index";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import TreeView from "components/TreeView";
 import { treeData } from "components/TreeView/mockTreeData";
 
+import { makeServer } from "./server";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
+}
 export default function App() {
+  useEffect(() => {
+    const url = `/api/users`;
+
+    fetch(url).then((res) => console.log(res));
+  }, []);
   return (
     <>
       <GlobalStyle />
